47836cb1e9de05da64bd0b876f24f8c51cb2a50a
silvia0223y <silvia0223y@gmail.com>
2017-05-04 22:26:00 -0400

    finish list book feature

---
+++ mybook_store/db/migrate/20170505020652_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.text :introduction
+      t.string :writer_name
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
