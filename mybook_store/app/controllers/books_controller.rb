a9820690bb1a8736d9792bd78a7ae52cc843ceac
silvia0223y <silvia0223y@gmail.com>
2017-05-04 22:37:33 -0400

    finish add book feature

--- mybook_store/app/controllers/books_controller.rb
+++ mybook_store/app/controllers/books_controller.rb
@@ -2,4 +2,20 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+      if @book.save
+        redirect_to books_path, notice: "新增書本成功!"
+      else
+        render :new
+      end
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:name, :introduction, :writer_name, :price)
+  end
 end
